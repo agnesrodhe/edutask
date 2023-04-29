b9ec06e148c3622412330b9519b5b6cdd4b3bad0
Vincent Wistrand <vincentwistrand@hotmail.se>
2023-04-29 09:00:09 +0200

    Add test

---
+++ frontend/cypress/e2e/R8UC3.cy.js
@@ -0,0 +1,73 @@
+describe('R8UC3 testing item deletion', () => {
+    let uid
+    let name
+    let taskTitle
+  
+    before(function () {
+      cy.fixture('user.json')
+        .then((user) => {
+          cy.request({
+            method: 'POST',
+            url: 'localhost:5001/users/create',
+            form: true,
+            body:user
+          }).then((response) => {
+            uid = response.body._id.$oid;
+            name = user.firstName + ' ' + user.lastName;
+          })
+        })
+      
+        cy.fixture('task.json')
+          .then((task) => {
+            task["userid"] = uid;
+              cy.request({
+                  method: 'POST',
+                  url: 'localhost:5001/tasks/create',
+                  form: true,
+                  body: task
+              }).then((response) => {
+                taskTitle = response.body[0].title;
+              })
+          })
+      })
+  
+    beforeEach(function() {
+      cy.visit('localhost:3000')
+
+      cy.contains('div', 'Email Address')
+      .find('input[type=text]')
+      .type('test@testande.com')
+  
+      cy.get('form')
+      .submit()
+  
+      cy.get('.container-element').eq(0)
+      .find('a')
+      .click()
+    })
+  
+    it('start on the landing page', () => {
+      cy.get('h1')
+      .find('span')
+      .should('contain.text', taskTitle)
+    })
+  
+    it('If user clicks on the x symbol behind the description of the todo item, the todo item is deleted', () => {
+      cy.get('.todo-item').eq(0)
+      .find('span').eq(1)
+      .click()
+
+      cy.get('.todo-list')
+      .should('not.have.class', 'todo-item')
+    })
+  
+    after(function () {
+      cy.request({
+        method: 'DELETE',
+        url: `localhost:5001/users/${uid}`
+      }).then((response) => {
+        cy.log(response.body)
+      })
+    })
+  
+  })
